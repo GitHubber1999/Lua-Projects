e89666ecfb3eb0bc44eadf1c02a8564ba27f5f13
GitHubber1999 <100528000+GitHubber1999@users.noreply.github.com>
2023-02-17 19:00:32 +0000

    Create antiaimview.lua

---
+++ antiaimview.lua
@@ -0,0 +1,4 @@
+hookfunction(game.Players.LocalPlayer.IsInGroup, function() return true end)
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "bypassed group!", Text = "wrd", Duration = 4,});
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nosssa/NossLock/main/AntiAimViewer"))()
